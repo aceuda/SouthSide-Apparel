255fab06fe46bf15b12decf59a9ea5333731347b
aceuda <acedenver1@gmail.com>
2025-10-31 00:07:52 +0800

    Initial commit - Spring Boot + React project setup

---
+++ src/main/java/com/southsideapparel/southsideapparel/SouthsideapparelApplication.java
@@ -0,0 +1,13 @@
+package com.southsideapparel.southsideapparel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SouthsideapparelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SouthsideapparelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/southsideapparel/southsideapparel/SouthsideapparelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.southsideapparel.southsideapparel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SouthsideapparelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
